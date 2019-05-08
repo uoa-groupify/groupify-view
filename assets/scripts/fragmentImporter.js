97eb4c19fae330068746885973ee269faa815e67
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-05-09 11:29:14 +1200

    add fragment loader

---
+++ assets/scripts/fragmentImporter.js
@@ -0,0 +1,19 @@
+const FRAGMENT_TAG = "Fragment";
+const FRAGMENT_ATTR_NAME = "name";
+
+/**
+ * Will replace <Fragment name="name"> with
+ * their fragment from the fragments folder
+ * @param element
+ */
+function insertFragments(element) {
+	$(FRAGMENT_TAG).each(function(index, element) {
+		//turns html element into a jquery element
+		var fragmentPlaceholder = $(element);
+		//Gets the content of the "name" attribute
+		var fragmentName = fragmentPlaceholder.attr(FRAGMENT_ATTR_NAME);
+
+		//insert fragment
+		fragmentPlaceholder.load("../fragments/" + fragmentName + ".html");
+	});
+}
